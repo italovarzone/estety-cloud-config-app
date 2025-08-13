308e87ed3691b6c1388da3722b15d10132c6c25e
Italo <italovarza@gmail.com>
2025-08-13 15:10:35 -0300

    feat: Ajusta endpoint prod.

--- app/api/auth/logout/route.js
+++ app/api/auth/logout/route.js
@@ -3,5 +3,5 @@ import { clearAuthCookie } from "../../../../lib/auth";
 
 export async function GET() {
   clearAuthCookie();
-  return NextResponse.redirect(new URL("/login", process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:4000"));
+  return NextResponse.redirect(new URL("/login", process.env.NEXT_PUBLIC_BASE_URL || "https://estety-cloud-config-app.vercel.app/"));
 }
