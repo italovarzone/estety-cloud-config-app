5c00034ac1209d3c5961b95c77508a42d73232a2
Ítalo <italovarza@gmail.com>
2025-11-04 11:52:43 -0300

    fix: remove default VAPID_SUBJECT email address for push notifications

--- lib/push.ts
+++ lib/push.ts
@@ -3,7 +3,7 @@ import { getDb } from "./mongo";
 
 const VAPID_PUBLIC_KEY = process.env.VAPID_PUBLIC_KEY || "";
 const VAPID_PRIVATE_KEY = process.env.VAPID_PRIVATE_KEY || "";
-const VAPID_SUBJECT = process.env.VAPID_SUBJECT || "mailto:ital.varzah@gmail.com";
+const VAPID_SUBJECT = process.env.VAPID_SUBJECT || "";
 
 let configured = false;
 function ensureConfigured() {
